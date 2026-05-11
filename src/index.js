ebcdf5bb52298f3d9d3a8de1f941b03654cee28f
dlh-bombana <dlh.nolimit@gmail.com>
2026-05-11 21:59:43 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Pastikan Anda menggunakan Tailwind CSS atau CSS biasa
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
